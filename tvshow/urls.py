085e7ea091eff549f828845022cf601c07c6703d
TELIDAS <adilet732@gmail.com>
2022-05-21 21:55:14 +0600

    fourth lesson

--- tvshow/urls.py
+++ tvshow/urls.py
@@ -5,6 +5,17 @@ from . import views
 app_name = "shows_url"
 urlpatterns = [
     path('shows/', views.tv_show, name="shows_all_url"),
+    path('shows/latest/', views.tv_show_latest, name="latest"),
+    path('shows/drama/', views.tv_show_drama, name="drama"),
+    path('shows/horror/', views.tv_show_horror, name="horror"),
+    path('shows/comedy/', views.tv_show_comedy, name="comedy"),
+    path('shows/fantasy/', views.tv_show_fantasy, name="fantasy"),
+    path('shows/scifi/', views.tv_show_sci_fi, name="scifi"),
+    path('shows/onime/', views.tv_show_onime, name="onime"),
+    path('shows/romantic/', views.tv_show_romantic, name="romantic"),
+    path('shows/action/', views.tv_show_action, name="action"),
     path('shows/<int:id>/', views.get_show_detail, name="show"),
+    path('shows/<int:id>/update/', views.show_update, name="show_update"),
+    path('shows/<int:id>/delete/', views.show_delete, name="show_delete"),
     path('add-shows/', views.add_show, name="add_show"),
 ]
--- tvshow/views.py
+++ tvshow/views.py
@@ -1,10 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from . import models, forms
+from datetime import datetime, timedelta
+
+start_date = datetime.today() - timedelta(days=5)
 
 
 def tv_show(request):
-    queryset = models.Shows.objects.all()
+    queryset = models.Shows.objects.order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_latest(request):
+    queryset = models.Shows.objects.filter(created_date__gt=start_date).order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_drama(request):
+    queryset = models.Shows.objects.filter(genre="Drama").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_horror(request):
+    queryset = models.Shows.objects.filter(genre="Horror").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_comedy(request):
+    queryset = models.Shows.objects.filter(genre="Comedy").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_fantasy(request):
+    queryset = models.Shows.objects.filter(genre="Fantasy").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_sci_fi(request):
+    queryset = models.Shows.objects.filter(genre="Sci-Fi").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_onime(request):
+    queryset = models.Shows.objects.filter(genre="Anime").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_romantic(request):
+    queryset = models.Shows.objects.filter(genre="Romantic").order_by("-id")
+    return render(request, "shows_all.html", {"shows": queryset})
+
+
+def tv_show_action(request):
+    queryset = models.Shows.objects.filter(genre="Action").order_by("-id")
     return render(request, "shows_all.html", {"shows": queryset})
 
 
@@ -24,3 +72,22 @@ def add_show(request):
     else:
         form = forms.TVShowForm()
     return render(request, "add_shows.html", {"form": form})
+
+
+def show_update(request, id):
+    show_object = get_object_or_404(models.Shows, id=id)
+    if request.method == "POST":
+        form = forms.TVShowForm(instance=show_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("shows_url:shows_all_url"))
+    else:
+        form = forms.TVShowForm(instance=show_object)
+    return render(request, "show_update.html", {"form": form,
+                                                "object": show_object})
+
+
+def show_delete(request, id):
+    show_object = get_object_or_404(models.Shows, id=id)
+    show_object.delete()
+    return redirect(reverse("shows_url:shows_all_url"))
